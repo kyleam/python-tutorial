64483ce5a5c3cdfda384cfc40b2d10dfb21d6148
Kyle Meyer <meyerkya@gmail.com>
2013-05-30 15:35:08 -0400

    gene coords task: add with and gtf match

--- code/get_gene_coords.py
+++ code/get_gene_coords.py
@@ -5,14 +5,27 @@ usage: python get_gene_coords.py
 
 gene_file = '../data/genes-5random.txt'  # REV
 
-infh = open(gene_file)  # REV
-filecontent = infh.read()
-infh.close()  # REV
+with open(gene_file) as infh:
+    filecontent = infh.read()
 
 filecontent = filecontent.strip()  # remove trailing new line
-
 genes = filecontent.split('\n')
 
 ## now we have genes of interest in list `genes`
 ## need to read in gtf file, find matching lines with matching genes,
 ## and extract coordinates
+
+gencode_file = '../data/gencode-v10-50random.gtf'
+with open(gencode_file) as genfh:
+    for line in genfh:
+        fields = line.split('\t')  ## newline -> \n
+        chrm = fields[0]  ## REV
+        start = fields[3]
+        stop = fields[4]
+        geneinfo = fields[8]
+
+        for desired_gene in genes:
+            genematch = '"' + desired_gene + '"'  ## REV
+            if genematch in geneinfo:
+                print(desired_gene, chrm, start, stop)
+                break  ## "continue" goes to next iteration
