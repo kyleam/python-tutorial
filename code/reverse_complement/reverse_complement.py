198b66550ef13854ac8ae8d0328f922f8ddd585a
Kyle Meyer <meyerkya@gmail.com>
2013-06-20 22:49:40 -0400

    add docstring to reverse complement function

--- code/reverse_complement/reverse_complement.py
+++ code/reverse_complement/reverse_complement.py
@@ -2,6 +2,13 @@
 
 
 def reverse_complement(sequence):
+    """Return the reverse complement of `sequence`
+
+    Parameters
+    ----------
+    sequence : iterable  ## REV: why not just call this a `str`?
+        can consist of G, C, A, or T
+    """
     basemap = {
         'A': 'T',
         'T': 'A',
@@ -11,5 +18,7 @@ def reverse_complement(sequence):
     return ''.join([basemap[base] for base in reversed(sequence)])
 
 if __name__ == '__main__':
-    result =  reverse_complement('ATGC')
-    assert result == 'GCAT'
+    assert reverse_complement('ATGC') == 'GCAT'
+
+    ## a list works, too
+    assert  reverse_complement(['A', 'T', 'G', 'C']) == 'GCAT'
