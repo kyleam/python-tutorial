3720870b9a7bc0b2f1dd3b68982f7a23ef852917
Kyle Meyer <meyerkya@gmail.com>
2013-07-12 18:42:10 -0400

    remove theme loading from publish.el
    not necessary because setting org-html-htmlize-output-type to css

--- publish.el
+++ publish.el
@@ -1,9 +1,6 @@
 (add-to-list 'load-path (car (file-expand-wildcards "~/.emacs.d/elpa/org-*")))
 (add-to-list 'load-path (car (file-expand-wildcards "~/.emacs.d/elpa/htmlize-*")))
 
-(setq custom-theme-directory "~/.emacs.d/theme")
-(load-theme 'barely)
-
 (require 'org)
 (require 'htmlize)
 
